ebd3e02bbba179eb503b9426e0e0f79306937cb9
nagar divy <nagardivya73@gmail.com>
2026-04-30 01:28:35 +0530

    fix all db files2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, session, render_template
+from flask import Flask
 from auth.routes import auth
 from resume.detail import resume
 from admin.routes import admin
@@ -10,7 +10,3 @@ app.secret_key = "register_form"
 app.register_blueprint(auth)
 app.register_blueprint(resume)
 app.register_blueprint(admin)
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
